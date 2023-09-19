73c29c4a1e186c749bfc02a8bd41bdc2e8403ef0
Dennis Byberg <dennis.byberg@hotmail.com>
2023-09-19 14:30:12 +0200

    Created header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,21 @@
 import "./Header.scss";
+import dictionaryPNG from "../../assets/images/dictionary.png";
+import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 
 function Header() {
-  return <header>Header</header>;
+  const dictionaryALT = "A purple logo of the site dictionary";
+  const dictionaryName = "Dictionary";
+
+  return (
+    <header className="header">
+      <img
+        className="header__dictionary-png"
+        src={dictionaryPNG}
+        alt={dictionaryALT}
+      />
+      <h1 className="header__dictionary-name">{dictionaryName}</h1>
+      <DarkModeToggle />
+    </header>
+  );
 }
 export default Header;
