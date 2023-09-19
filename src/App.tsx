e81a0022cafadbb1d19c327c9e3153097a41b3e9
Dennis Byberg <dennis.byberg@hotmail.com>
2023-09-19 14:28:15 +0200

    added change dark mode context

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,23 @@ import { createContext, useState } from "react";
 import "./App.scss";
 import Header from "./components/Header/Header";
 
-const DarkModeContext = createContext<IDarkModeContext | undefined>(undefined);
+const DarkModeContext = createContext<TDarkModeContext | undefined>(undefined);
+const ChangeDarkModeContext = createContext<TChangeDarkModeContext | undefined>(
+  undefined
+);
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState<boolean>(true);
 
   return (
     <div className={isDarkMode ? "App" : "App light-mode "}>
-      <DarkModeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
-        <Header />
+      <DarkModeContext.Provider value={isDarkMode}>
+        <ChangeDarkModeContext.Provider value={setIsDarkMode}>
+          <Header />
+        </ChangeDarkModeContext.Provider>
       </DarkModeContext.Provider>
     </div>
   );
 }
 
-export { App, DarkModeContext };
+export { App, DarkModeContext, ChangeDarkModeContext };
