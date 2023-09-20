441c0b91cab840b56afd3b6acce13613540072f5
Dennis Byberg <dennis.byberg@hotmail.com>
2023-09-20 02:54:42 +0200

    updated searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createContext, useState } from "react";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Searchbar from "./components/Searchbar/Searchbar";
+import WordList from "./components/WordList/WordList";
 
 const DarkModeContext = createContext<TDarkModeContext | undefined>(undefined);
 const ChangeDarkModeContext = createContext<TChangeDarkModeContext | undefined>(
@@ -10,13 +11,15 @@ const ChangeDarkModeContext = createContext<TChangeDarkModeContext | undefined>(
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState<boolean>(true);
+  const [searchWord, setSearchWord] = useState<string>("");
 
   return (
     <div className={isDarkMode ? "App" : "App light-mode "}>
       <DarkModeContext.Provider value={isDarkMode}>
         <ChangeDarkModeContext.Provider value={setIsDarkMode}>
           <Header />
-          <Searchbar />
+          <Searchbar setSearchWord={setSearchWord} />
+          <WordList searchWord={searchWord} />
         </ChangeDarkModeContext.Provider>
       </DarkModeContext.Provider>
     </div>
--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,19 +1,22 @@
 import "./Searchbar.scss";
 import searchPNG from "../../assets/images/search.png";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { DarkModeContext } from "../../App";
 
-function Searchbar() {
+function Searchbar({ setSearchWord }: ISearchbarProps) {
   const isDarkMode = useContext(DarkModeContext);
+  const [searchInput, setSearchInput] = useState<string>("");
 
   return (
     <div className="searchbar">
       <input
+        onChange={(e) => setSearchInput(e.target.value)}
         className={`searchbar__input${isDarkMode ? "" : " light-mode"}`}
         placeholder="search..."
         type="text"
       />
       <img
+        onClick={() => setSearchWord(searchInput)}
         className={`searchbar__searchPNG${isDarkMode ? "" : " light-mode"}`}
         src={searchPNG}
       />
