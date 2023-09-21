5b9280cd81aad942a48d06404629e16cfc801910
Dennis Byberg <dennis.byberg@hotmail.com>
2023-09-21 23:58:37 +0200

    added display favorites

--- src/components/DisplayFavorites/DisplayFavorites.tsx
+++ src/components/DisplayFavorites/DisplayFavorites.tsx
@@ -1,11 +1,23 @@
 import { useContext } from "react";
 import "./DisplayFavorites.scss";
-import { FavoriteWord } from "../../context/FavoriteWordContextProvider.js";
+import { FavoriteWord } from "../../context/FavoriteWordContextProvider.tsx";
 
 function DisplayFavorites() {
-  const { words } = useContext(FavoriteWord);
-  // console.log(words);
+  const { favoritedWord } = useContext(FavoriteWord);
 
-  return <div></div>;
+  return (
+    <div className="display-favorites">
+      <h1>Favorites</h1>
+      <ul>
+        {favoritedWord.length > 0 &&
+          favoritedWord.map((favWord: any) => (
+            <li key={favWord.id}>
+              <h2> {favWord.favoritedWord[0].word} </h2>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
+
 export default DisplayFavorites;
