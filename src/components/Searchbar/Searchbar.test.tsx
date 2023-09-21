4d74ee2257f7b4ff78df0f33e3d5d66ae746b979
Dennis Byberg <dennis.byberg@hotmail.com>
2023-09-21 16:59:28 +0200

    added vitest testing

---
+++ src/components/Searchbar/Searchbar.test.tsx
@@ -0,0 +1,32 @@
+import { describe, it, expect, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Searchbar from "./Searchbar";
+import userEvent from "@testing-library/user-event";
+
+describe(Searchbar, () => {
+  it("Should render searchbar", () => {
+    render(<Searchbar setWordObject={vi.fn()} />);
+  });
+
+  it("Should accept a user typing", async () => {
+    render(<Searchbar setWordObject={vi.fn()} />);
+    const user = userEvent.setup();
+    const input = screen.getByRole("textbox");
+
+    await user.type(input, "hello");
+    expect(input).toHaveValue("hello");
+  });
+
+  it("SearchInput is empty after a search...", async () => {
+    render(<Searchbar setWordObject={vi.fn()} />);
+    const searchBtn = screen.getByAltText("searchPNG");
+    const input = screen.getByRole("textbox");
+    const user = userEvent.setup();
+
+    await user.type(input, "hello");
+    expect(input).toHaveValue("hello");
+
+    await user.click(searchBtn);
+    expect(input).toHaveValue("");
+  });
+});
