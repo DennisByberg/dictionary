ed69699af7f5c111ba456be6a61d548e487e0893
Dennis Byberg <dennis.byberg@hotmail.com>
2023-09-26 18:10:38 +0200

    implemented tests for header

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,24 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Header from "./Header";
+
+describe(Header, () => {
+  it("should render the Header component without errors", () => {
+    render(<Header />);
+  });
+
+  it("Should render the logo correctly", () => {
+    render(<Header />);
+    const logo = screen.getByAltText("A purple logo of the site dictionary");
+
+    expect(logo).toBeInTheDocument();
+  });
+
+  it("Should display the correct header text", () => {
+    render(<Header />);
+    const headerText = screen.getByRole("heading", { level: 1 });
+
+    expect(headerText).toHaveTextContent("Dictionary");
+  });
+});
